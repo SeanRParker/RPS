c38e0c45914812cf5d47b0bb22e8c824b44c71d5
Shaws <seanrichardparker@gmail.com>
2021-12-29 10:29:29 -0500

    insert multiple rounds logic

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
-let playerSelection = () => {
-  return window.prompt("rock, paper, or scissors?").toLowerCase();
+let playerPick = (msg = "rock, paper, or scissors") => {
+  let pick = window.prompt(msg).toLowerCase();
+
+  if (pick === 'rock' || pick === 'paper' || pick === 'scissors') {
+    return pick;
+  } else {
+    console.log('not allowed! Pick one!');
+    return playerPick("Try again. Rock, Paper, or Scissors!");
+  }
 }
 
-let computerPlay = () => {
+let computerPick = () => {
   let randomValue = Math.random() * 3;
-  console.log('randomVal: ', randomValue);
   if (randomValue < 1) {
     return "rock";
   } else if (randomValue < 2) {
@@ -19,18 +25,40 @@ let capitalize = (str) => {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
-console.log(computerPlay());
+console.log(computerPick());
 
-let play = (pSel, cSel) => {
+let playerScore = 0;
+let computerScore = 0;
+let roundNum = 0;
+
+let playRound = (pSel, cSel, rounds) => {
   if (pSel === "rock" && cSel === "paper" || pSel === "paper" && cSel === "scissors" || pSel === "scissors" && cSel === "rock") {
       console.log(`You Lose! ${capitalize(cSel)} beats ${pSel}`);
+      computerScore += 1;
+      console.log(`${playerScore === rounds || computerScore === rounds ? 'FINAL' : 'CURRENT'} SCORE: Computer - ${computerScore} | You - ${playerScore}`);
+      roundNum += 1;
   } else if(pSel === cSel) {
     console.log("Tie! Play again");
-    play(playerSelection(), computerPlay());
+    playRound(playerPick(), computerPick());
   } else {
     console.log(`You Win! ${capitalize(pSel)} beats ${cSel}`);
+    playerScore += 1;
+    console.log(`${playerScore === rounds || computerScore === rounds ? 'FINAL' : 'CURRENT'} SCORE: You - ${playerScore} | Computer - ${computerScore}`);   
+    roundNum += 1;
   }
 }
 
+let game = (rounds) => {
 
-play(playerSelection(), computerPlay());
+  while(playerScore < rounds && computerScore < rounds) {
+    playRound(playerPick(), computerPick(), rounds);
+  }
+
+  if(playerScore === rounds){
+    console.log("You WON! Good Job!");
+  } else {
+    console.log("You lose. Better luck next time.");
+  }
+}
+
+game(5);
